4de82f59f14fba45e6f6130999ffc3583d3adbc3
Nishad Menezes <nishadmenezes@gmail.com>
2022-06-30 22:01:41 +0530

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7C5B43D3D075B575E99Dea655021f6F6a24A4123";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
